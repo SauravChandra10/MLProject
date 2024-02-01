6d7e35a4c709625b1952d1760f5bd24378c44798
SauravChandra10 <sauravchandra10@gmail.com>
2024-02-01 21:01:51 +0530

    flask app and prediction pipeline

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
+from flask import Flask, request, render_template
 import sys
-from src.MLProject.logger import logging 
-from src.MLProject.exception import CustomException 
-from src.MLProject.components.data_ingestion import DataIngestion
-from src.MLProject.components.data_transformation import DataTransformation
-from src.MLProject.components.model_trainer import ModelTrainerConfig, ModelTrainer
+from src.MLProject.exception import CustomException
+from src.MLProject.pipelines.prediction_pipeline import PredictPipeline, CustomData
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    try:
+        f = request.files['data_file']
+    except Exception as e:
+        raise CustomException(e,sys)
+
+    data = CustomData(f).df
+
+    prediction = PredictPipeline().predict(data)
+
+    return render_template("index.html",prediction=prediction)
 
 if __name__ == "__main__":
-    logging.info("The execution has started")
-
-    try:
-        data_ingestion = DataIngestion()
-        train_path,test_path=data_ingestion.initiate_data_ingestion()
-        
-        data_transformation = DataTransformation()
-        train_arr, test_arr = data_transformation.initiate_data_transormation(train_path,test_path)
-
-        model_trainer = ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
+    app.run(debug=True)
---
+++ src/MLProject/pipelines/prediction_pipeline.py
@@ -0,0 +1,44 @@
+import sys, os
+import io
+from io import StringIO
+import pandas as pd
+from src.MLProject.exception import CustomException
+from src.MLProject.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            model=load_object(file_path=model_path)
+            preds=model.predict(features)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+class CustomData:
+    def __init__(self,f):
+        stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+        stream.seek(0)
+        result = stream.read()
+        df = pd.read_csv(StringIO(result))
+
+        df['date'] = pd.to_datetime(df['Date'], format = '%d-%m-%Y')
+        df['Year'] = df['date'].dt.year
+        df['Day'] = df['date'].dt.day
+        df=df.drop(columns=['Date','date'],axis=1)
+
+        self.df=df
+
+    def get_data_as_data_frame(self):
+        try:
+            return self.df
+
+        except Exception as e:
+            raise CustomException(e, sys)
